090d8ddac9adafd8178d20c59b769c83d3380535
v <vishal.gautam@sofdesk.com>
2021-01-22 13:31:28 -0500

    initial commit

--- src/app/core.cljs
+++ src/app/core.cljs
@@ -1,14 +1,20 @@
 (ns app.core
   "This namespace contains your application and is the entrypoint for 'yarn start'."
-  (:require [reagent.core :as r]
-            [app.hello :refer [hello]]))
+  (:require [reagent.core :as r]))
+
+(defn app []
+  ;; vector -> data
+  [:h1 "Hello, World!"])
 
 (defn ^:dev/after-load render
-  "Render the toplevel component for this app."
+  "Render the toplevel component for this app.
+   Dev Mode - runs every time the required dependencies changes"
   []
-  (r/render [hello] (.getElementById js/document "app")))
+  (r/render [app] (.getElementById js/document "app")))
 
 (defn ^:export main
-  "Run application startup logic."
+  "Run application startup logic. Runs only once"
   []
   (render))
+
+(js/console.log "Conduit")
